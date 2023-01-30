cd30655f2f8d50f34b73c722b9854abc04e6a2d4
Jason Liu <lxjsilver@gmail.com>
2023-01-30 15:49:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -8,17 +8,17 @@ const BLOG = {
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
   LANG: 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: 2021, // e.g if leave this empty, current year will be used.
-  APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
+  APPEARANCE: 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   AUTHOR: 'psikent', // 作者
-  BIO: '电气工程师🍚', // 作者简介
+  BIO: '电气工程师', // 作者简介
   LINK: 'https://lxj.ink', // 网站地址
   KEYWORDS: '电气,CS', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: 'lxjsilver@gmail.com', // 邮箱
   CONTACT_WEIBO: '', // 你的微博个人主页
   CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB: 'https://github.com/psikent', // 你的github个人主页
+  CONTACT_GITHUB: '', // 你的github个人主页
   CONTACT_TELEGRAM: '', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
